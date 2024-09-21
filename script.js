9ab9b79994d1ed3ad8660af12d0f7f936ac235a6
AdrianGM-code <20650@virtual.utsc.edu.mx>
2024-09-20 23:42:25 -0600

    Primer Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#productsTable').DataTable({
+        "paging": true,
+        "searching": true,
+        "ordering": true,
+        "info": true
+    });
+});
